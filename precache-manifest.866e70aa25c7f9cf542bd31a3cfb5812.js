f14bddbc5c9389573c1af908ef25de1098b3ac2c
Pat-Lim <impatlim@gmail.com>
2020-04-27 15:34:00 +1200

    Updates

---
+++ precache-manifest.866e70aa25c7f9cf542bd31a3cfb5812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b08cf2b398587a73b61e04d64e326d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564acf57478b02f90734",
+    "url": "/static/css/main.04db46fd.chunk.css"
+  },
+  {
+    "revision": "2209d8504d14b6d69995",
+    "url": "/static/js/2.47877bb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.47877bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564acf57478b02f90734",
+    "url": "/static/js/main.41d6cce1.chunk.js"
+  },
+  {
+    "revision": "f3dc2a2ba24c62abd98b",
+    "url": "/static/js/runtime-main.1c046400.js"
+  }
+]);
